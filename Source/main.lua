53bf77379b6be64bd1a415d687c214a9ead15892
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2022-03-15 16:46:09 +1000

    Initial commit

---
+++ Source/main.lua
@@ -0,0 +1,77 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "pd_vertlet"
+
+local gfx = playdate.graphics
+local gravity           = 450
+local cloth_height      = 30
+local cloth_width       = 1
+local start_y           = 0
+local spacing           = 7
+local tear_distance     = 80
+local rope_length       = 20
+local dt                = 0.05 -- for now, assume 20fps constant
+
+gfx.setColor(gfx.kColorBlack)
+
+function build_rope()
+  local points = {}
+  local start_x = 200 -- love.graphics.getWidth()/2
+  
+  for y = 1, rope_length do
+    local p = Point(start_x, start_y + (y * spacing) - spacing)
+    
+    if y ~= 1 then
+      p:attach(points[(y-2) + 1], spacing, tear_distance)
+    end
+    
+    if y == 1 then
+      p:pin(p.x, p.y)
+    end
+    
+    table.insert(points, p)
+  end
+  return points
+end
+
+function load()
+  playdate.startAccelerometer()
+  calibrated_accelerometer = {playdate.readAccelerometer()}
+  print (calibrated_accelerometer[1], calibrated_accelerometer[2],calibrated_accelerometer[3])
+  points = build_rope()
+  once = true
+  physics = Physics(points);
+
+  imageTablePlayer = gfx.imagetable.new("diverDingus-table-32-32.png")
+  imagePlayer = imageTablePlayer:getImage(2, 1)
+  imageBackground = gfx.image.new("backgroundTest.png")
+end
+
+load()
+
+function playdate.update(dt)
+  
+  -- Get info from accelerometer and add force to the rope points
+  physics:update(dt)
+  
+  -- put the dangling man on the end of the rope
+  playerX, playerY = points[rope_length].x - 16, points[rope_length].y - 6
+  
+  -- Draw the rope & dangling man
+  gfx.clear(gfx.kColorWhite)
+  imageBackground:draw(0, 0)
+  
+  gfx.setColor(gfx.kColorBlack)
+  
+  for index, point in ipairs(points) do
+    point:draw()
+  end
+  
+  gfx.setColor(gfx.kColorWhite)
+  imagePlayer:draw(playerX, playerY)
+  playdate.drawFPS(0,0)
+
+end
+
---
+++ Source/pd_vertlet.lua
@@ -0,0 +1,184 @@
+import 'coreLibs/object'
+import 'coreLibs/graphics'
+
+local gravity           = 450
+local physics_accuracy  = 3 -- how many passes should the constraint solver do? 1 is fine but stretchy.
+local sqrt = math.sqrt
+local mouse_influence   = 20
+local mouse_cut         = 10
+local screen_height     = 240
+local screen_width      = 400
+local accel_threshold   = 0.01
+local accel_multiplier  = 100
+local dt                = 0.05 -- for now, assume 20fps constant
+local gfx = playdate.graphics
+
+class('Constraint').extends()
+
+function Constraint:init(p1, p2, spacing, tear_distance)
+  self.p1 = p1
+  self.p2 = p2
+  self.length = spacing
+  self.tear_distance = tear_distance
+end
+
+function Constraint:solve()
+  local diff_x = self.p1.x - self.p2.x
+  local diff_y = self.p1.y - self.p2.y
+  local dist = sqrt(diff_x * diff_x + diff_y * diff_y)
+  local diff = (self.length - dist) / dist
+
+  if dist > self.tear_distance then
+	self.p1:remove_constraint(self)
+  end
+
+  local scalar_1 = (1 / self.p1.mass) / ((1 / self.p1.mass) + (1 / self.p2.mass))
+  local scalar_2 = 1 - scalar_1
+
+  self.p1.x = self.p1.x + diff_x * scalar_1 * diff
+  self.p1.y = self.p1.y + diff_y * scalar_1 * diff
+
+  self.p2.x = self.p2.x - diff_x * scalar_2 * diff
+  self.p2.y = self.p2.y - diff_y * scalar_2 * diff
+end
+
+
+function Constraint:draw()
+  gfx.setLineWidth(2)
+  gfx.drawLine(self.p1.x, self.p1.y, self.p2.x, self.p2.y)
+end
+
+
+class('Point').extends()
+
+function Point:init(x, y)
+  self.x = x
+  self.y = y
+  self.px = x
+  self.py = y
+  self.ax = 0
+  self.ay = 0
+  self.mass = 1
+  self.constraints = {}
+  self.pinned = false
+  self.pin_x = nil
+  self.pin_y = nil
+end
+
+function Point:update(dt)
+  local dt = .05
+  self:add_force(0, self.mass * gravity)
+  local vx = self.x - self.px
+  local vy = self.y - self.py
+  local delta = dt * dt
+
+  local nx = self.x + 0.99 * vx + 0.5 * self.ax * delta
+  local ny = self.y + 0.99 * vy + 0.5 * self.ay * delta
+
+  self.px = self.x
+  self.py = self.y
+
+  self.x = nx
+  self.y = ny
+
+  self.ax = 0
+  self.ay = 0
+end
+
+function Point:update_accelerometer(calibrated_accelerometer)
+  current_accelerometer = {playdate.readAccelerometer()}
+  x_force = calibrated_accelerometer[1] - current_accelerometer[1]
+  y_force = calibrated_accelerometer[2] - current_accelerometer[2]
+  
+  if math.abs(x_force) > accel_threshold or math.abs(y_force) > accel_threshold then
+	  self:add_force(-x_force * accel_multiplier, -y_force * accel_multiplier/2)
+  end
+  
+  --calibrated_accelerometer = current_accelerometer
+end
+  
+
+function Point:draw()
+  if #self.constraints == 0 then
+	return
+  end
+  for index, constraint in ipairs(self.constraints) do
+	constraint:draw()
+  end
+end
+
+function Point:solve_constraints()
+  for index, constraint in ipairs(self.constraints) do
+	constraint:solve()
+  end
+
+  if self.y < 1 then
+   self.y = 2 * (1) - self.y
+  elseif self.y > screen_height then
+	self.y = 2 * screen_height - self.y
+  end
+
+  if self.x > screen_width then
+   self.x = 2 * screen_width - self.x
+
+  elseif self.x < 1 then
+	self.x = 2 * 1 - self.x
+  end
+
+  -- Pinning
+  if self.pinned then
+	self.x = self.pin_x
+	self.y = self.pin_y
+  end
+end
+
+
+function Point:attach(P, spacing, tear_distance)
+  local constraint = Constraint(self, P, spacing, tear_distance)
+  table.insert(self.constraints, constraint)
+end
+
+function Point:remove_constraint(lnk)
+  for index, constraint in ipairs(self.constraints) do
+	if constraint == lnk then
+	  table.remove(self.constraints, index)
+	  return
+	end
+  end
+end
+
+function Point:add_force(fx, fy)
+  self.ax = self.ax + fx / self.mass
+  self.ay = self.ay + fy / self.mass
+end
+
+function Point:pin(px, py)
+  self.pinned = true
+  self.pin_x = px
+  self.pin_y = py
+end
+
+
+class('Physics').extends()
+
+function Physics:init(points)
+  self.points = points
+  self.delta_sec = 16 / 1000;
+  self.accuracy = physics_accuracy
+end
+
+function Physics:update(dt)
+  for i = self.accuracy, 0, -1 do
+	for index, point in ipairs(self.points) do
+	  point:update_accelerometer(calibrated_accelerometer)
+	  point:solve_constraints()
+	end
+  end
+
+  --points[#self.points].mass = 10
+  -- points[#self.points]:update_accelerometer(calibrated_accelerometer)
+
+  for index, point in ipairs(self.points) do
+	point:update(dt)
+  end
+end
